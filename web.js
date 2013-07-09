4f3386770ef99ae16b1b07cd1e7e45e47fa50dbf
MVS1 <o@moviusstudios.com>
2013-07-09 18:09:32 +0000

    New web.js

--- web.js
+++ web.js
@@ -1,14 +1,11 @@
-var express = require('express');
-
+var express = require('express'),
+var fs = require('fs'),
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-response.send('Hello World2!');
+  response.send(fs.readFileSync("index.html").toString());
 });
-fs.writeFile('message.txt', 'Hello Node', function (err) {
-  if (err) throw err;
-  console.log('It\'s saved!');
-});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
