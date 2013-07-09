f49cdf72ef56bd34145f247cccfa2ffb62afccbc
MVS1 <o@moviusstudios.com>
2013-07-09 15:57:47 +0000

    2nd update

--- web.js
+++ web.js
@@ -3,9 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+response.send('Hello World2!');
 });
-
+fs.writeFile('message.txt', 'Hello Node', function (err) {
+  if (err) throw err;
+  console.log('It\'s saved!');
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
